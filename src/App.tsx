827a628aa7b88b29cc6d386d5b11523ac42aaaa1
William Bettencourt <williambet@gmail.com>
2026-07-20 16:09:17 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Flex, Text, Button } from "@radix-ui/themes";
+
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <Flex direction="column" gap="2">
+			  <Text>Hello from Radix Themes :)</Text>
+			  <Button>Let's go</Button>
+		  </Flex>
+    </main>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Theme, ThemePanel } from "@radix-ui/themes";
+
+import '@radix-ui/themes/styles.css';
+
+import './index.css'
+
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Theme>
+      <App />
+      <ThemePanel />
+    </Theme>
+  </StrictMode>,
+)
